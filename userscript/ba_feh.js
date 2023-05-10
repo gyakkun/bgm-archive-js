a1e36cb2f3b8eece98b10b93052c15cbe0da5e45
gyakkun <xiaowufeixia@gmail.com>
2023-05-10 18:35:09 +0800

    it works!

--- userscript/ba_feh.js
+++ userscript/ba_feh.js
@@ -7,37 +7,213 @@
 // ==/UserScript==
 
 (function () {
+    const SPACE_TYPE = getSpaceType();
     const BA_API_URL = "https://bgm.nyamori.moe/forum-enhance/query"
     const BA_FEH_CACHE_PREFIX = "ba_feh_" + SPACE_TYPE + "_" // + username
     const FACE_KEY_GIF_MAPPING = {
-        0: 44,
-        140: 101,
-        80: 41,
-        54: 15,
-        85: 46,
-        104: 65,
-        88: 49,
-        62: 23,
-        79: 40,
-        53: 14,
-        122: 83,
-        92: 53,
-        118: 79,
-        141: 102,
-        90: 51,
-        76: 37,
-        60: 21,
-        128: 89,
-        47: 08,
-        68: 29,
-        137: 98,
-        132: 93
+        0: "44",
+        140: "101",
+        80: "41",
+        54: "15",
+        85: "46",
+        104: "65",
+        88: "49",
+        62: "23",
+        79: "40",
+        53: "14",
+        122: "83",
+        92: "53",
+        118: "79",
+        141: "102",
+        90: "51",
+        76: "37",
+        60: "21",
+        128: "89",
+        47: "08",
+        68: "29",
+        137: "98",
+        132: "93"
     }
-    const SPACE_TYPE = getSpaceType();
     const SPACE_ACTIO_BUTTON_WORDING = {
         "group": "小组统计",
         "subject": "条目统计"
     };
+    const mockObj = {
+        "_meta": {
+            "expiredAt": 1683693346597
+        },
+        "type": "group",
+        "postStat": {
+            "total": 1133,
+            "deleted": 1,
+            "adminDeleted": 0
+        },
+        "topicStat": {
+            "total": 32,
+            "deleted": 0,
+            "silent": 0,
+            "closed": 0,
+            "reopen": 0
+        },
+        "likeStat": {
+            "47": 2,
+            "118": 5,
+            "140": 48
+        },
+        "spaceStat": [
+            {
+                "name": "boring",
+                "displayName": "靠谱人生茶话会",
+                "post": {
+                    "total": 726,
+                    "deleted": 0,
+                    "adminDeleted": 0
+                },
+                "topic": {
+                    "total": 7,
+                    "deleted": 0,
+                    "silent": 0,
+                    "closed": 0,
+                    "reopen": 0
+                }
+            },
+            {
+                "name": "a",
+                "displayName": "～技术宅真可怕～",
+                "post": {
+                    "total": 80,
+                    "deleted": 0,
+                    "adminDeleted": 0
+                },
+                "topic": {
+                    "total": 0,
+                    "deleted": 0,
+                    "silent": 0,
+                    "closed": 0,
+                    "reopen": 0
+                }
+            },
+            {
+                "name": "fillgrids",
+                "displayName": "补旧番",
+                "post": {
+                    "total": 57,
+                    "deleted": 0,
+                    "adminDeleted": 0
+                },
+                "topic": {
+                    "total": 0,
+                    "deleted": 0,
+                    "silent": 0,
+                    "closed": 0,
+                    "reopen": 0
+                }
+            },
+            {
+                "name": "showdesktop",
+                "displayName": "不晒桌面就会死星人",
+                "post": {
+                    "total": 43,
+                    "deleted": 0,
+                    "adminDeleted": 0
+                },
+                "topic": {
+                    "total": 3,
+                    "deleted": 0,
+                    "silent": 0,
+                    "closed": 0,
+                    "reopen": 0
+                }
+            },
+            {
+                "name": "download",
+                "displayName": "宽带综合症候群",
+                "post": {
+                    "total": 37,
+                    "deleted": 0,
+                    "adminDeleted": 0
+                },
+                "topic": {
+                    "total": 0,
+                    "deleted": 0,
+                    "silent": 0,
+                    "closed": 0,
+                    "reopen": 0
+                }
+            }
+        ],
+        "recentActivities": {
+            "topic": [
+                {
+                    "title": "柯南CP投票",
+                    "id": 380148,
+                    "dateline": 1680886800
+                }
+            ],
+            "post": [
+                {
+                    "title": "[UNOFFICIAL] 某不靠谱的bangumi搜索功能",
+                    "mid": 381365,
+                    "pid": 2325318,
+                    "dateline": 1683295260
+                },
+                {
+                    "title": "点格子显示出问题？",
+                    "mid": 380624,
+                    "pid": 2308438,
+                    "dateline": 1681970820
+                },
+                {
+                    "title": "大二科班迷茫了，大佬们给点建议",
+                    "mid": 380560,
+                    "pid": 2307307,
+                    "dateline": 1681838580
+                },
+                {
+                    "title": "关于新出的绝交功能的样式BUG",
+                    "mid": 380314,
+                    "pid": 2300465,
+                    "dateline": 1681220580
+                },
+                {
+                    "title": "站点过期信息汇报",
+                    "mid": 366586,
+                    "pid": 2298989,
+                    "dateline": 1681117680
+                },
+                {
+                    "title": "柯南CP投票",
+                    "mid": 380148,
+                    "pid": 2296602,
+                    "dateline": 1680942720
+                },
+                {
+                    "title": "绝交功能出了",
+                    "mid": 380060,
+                    "pid": 2294086,
+                    "dateline": 1680712680
+                },
+                {
+                    "title": "（已解决）问一下win32程序和html学哪个好点",
+                    "mid": 379658,
+                    "pid": 2285874,
+                    "dateline": 1680005460
+                },
+                {
+                    "title": "[防护口罩交流]说出你预算",
+                    "mid": 379362,
+                    "pid": 2278111,
+                    "dateline": 1679282640
+                },
+                {
+                    "title": "话说昨天是有人发了个椒盐音乐的帖子然后删了吗？",
+                    "mid": 379015,
+                    "pid": 2268844,
+                    "dateline": 1678419000
+                }
+            ]
+        }
+    }
 
     function getSpaceType() {
         return document.location.pathname.split("/")[1];
@@ -55,15 +231,15 @@
         }
     }
 
-    function getAllUsername() {
+    function getAllUsernameSet() {
         var set = {}
-        getPostDivList.each(function () { set[$(this).attr("data-item-user")] = null })
-        return Object.keys(set)
+        getPostDivList().each(function () { set[$(this).attr("data-item-user")] = null })
+        return set
     }
 
     function drawWrapper(username, postId, userStatObj) {
         return `
-            <div id="ba-feh-wrapper-${postId}-${username}" class="subject_tag_section" style="margin: 1em;">
+            <div id="ba-feh-wrapper-${postId}-${username}" class="subject_tag_section" style="margin: 1em;float: inline-block">
                 <div>
                     <div id="ba-feh-post-stat-${postId}-${username}">
                         <span class="tip">帖子统计:</span>
@@ -84,6 +260,7 @@
                     <div id="ba-feh-recent-activities-${postId}-${username}">
                         <span>最近发表:</span>
                         ${drawRecentTopicSection(userStatObj.recentActivities.topic)}
+                        <br/>
                         <span>最近回复:</span>
                         ${drawRecentPostSection(userStatObj.recentActivities.post)}
                     </div>
@@ -95,8 +272,8 @@
     function drawActionButton(username, postId) {
         return `
         <div class="action">
-            <a href="javascript:void(0);" class="icon" title="${SPACE_ACTIO_BUTTON_WORDING}">
-                <span data-dropped="false" class="ico" id="ba-feh-action-btn-${postId}-${username}" style="text-indent: 0px">▼</span><span class="title">${SPACE_ACTIO_BUTTON_WORDING}</span>
+            <a href="javascript:void(0);" class="icon" title="${SPACE_ACTIO_BUTTON_WORDING[SPACE_TYPE]}">
+                <span data-dropped="false" class="ico" id="ba-feh-action-btn-${postId}-${username}" style="text-indent: 0px">▼</span><span class="title">${SPACE_ACTIO_BUTTON_WORDING[SPACE_TYPE]}</span>
             </a>
         </div>
         `
@@ -148,7 +325,7 @@
     }
 
     function drawRecentPost(postBriefObj) {
-        return `<a class="l inner" href="/group/topic/${postBriefObj.mid}/${postBriefObj.pid}">${postBriefObj.title} <small class="grey">${formatDateline(postBriefObj.dateline)}</small></a>`
+        return `<a class="l inner" href="/group/topic/${postBriefObj.mid}#post_${postBriefObj.pid}">${postBriefObj.title} <small class="grey">${formatDateline(postBriefObj.dateline)}</small></a>`
     }
 
     function drawRecentTopic(topicBriefObj) {
@@ -222,7 +399,7 @@
         for (key in faceMap) {
             res.push([key, faceMap[key]])
         }
-        res.sort((a, b) => { b[1] - a[1] })
+        res = res.sort((a, b) => b[1] - a[1])
         return res
     }
 
@@ -238,18 +415,62 @@
     function registerOnClickEvent() {
         $("span[id^='ba-feh-action-btn-'").each(function () {
             let that = $(this)
-            that.click(() => {
-                alert("hi");
+            let pid = that.attr("id").split("-")[4]
+            let username = that.attr("id").split("-")[5]
+            that.click(async () => {
+                // alert("hi");
                 if (that.attr("data-dropped") === "false") {
                     that.html("▲")
                     that.attr("data-dropped", "true")
+                    if ($(`#ba-feh-wrapper-${pid}-${username}`).length > 0) {
+                        $(`#ba-feh-wrapper-${pid}-${username}`).show()
+                    } else {
+                        let userStatObj = await getUserStatObj(username)
+                        $("#likes_grid_" + pid).after(drawWrapper(username, pid, userStatObj))
+                    }
                 } else {
                     that.html("▼")
-                    that.attr("data-dropped", "true")
+                    that.attr("data-dropped", "false")
+                    $(`#ba-feh-wrapper-${pid}-${username}`).hide()
                 }
             })
 
         })
+    }
+
+    async function getUserStatObj(username) {
+        if (doesThisUserHasCache(username)) return getCacheByUsername(username)
+        let allUsernameSet = getAllUsernameSet()
+        for (un in allUsernameSet) {
+            if (doesThisUserHasCache(un))
+                delete allUsernameSet[un]
+        }
+        let usernameListToFetch = Object.keys(allUsernameSet)
+        let fetched = await fetch(BA_API_URL, {
+            body: JSON.stringify({ users: usernameListToFetch, type: SPACE_TYPE }),
+            method: "POST"
+        }).then(d => d.json())
+            .catch(e => console.error("Exception when fetching data: " + e, e))
+        for (u in fetched) {
+            sessionStorage[`${BA_FEH_CACHE_PREFIX}${u}`] = JSON.stringify(fetched[u])
+        }
+        return getCacheByUsername(username)
+    }
+
+
+    function doesThisUserHasCache(username) {
+        if (!!sessionStorage[`${BA_FEH_CACHE_PREFIX}${username}`]) {
+            let statObj = JSON.parse(sessionStorage[`${BA_FEH_CACHE_PREFIX}${username}`])
+            if ((new Date().valueOf()) > (statObj?._meta?.expiredAt ?? (new Date().valueOf()))) {
+                return false
+            }
+            return true
+        }
+        return false
+    }
+
+    function getCacheByUsername(username) {
+        return JSON.parse(sessionStorage[`${BA_FEH_CACHE_PREFIX}${username}`])
     }
 
     function formatDateline(dateline /* epoch seconds */) {
@@ -259,115 +480,5 @@
     }
     attachActionButton()
     registerOnClickEvent()
+
 })();
-
-
-
-
-const API_URL = "aHR0cHM6Ly9lYXN0YXNpYS5henVyZS5kYXRhLm1vbmdvZGItYXBpLmNvbS9hcHAvcHJldnBvc3QtanB6cW0vZW5kcG9pbnQvaGlzdG9yeT8="
-const PAYLOAD = "eyJhcGkta2V5IiA6ICJNZjQ3eDBsMmtUajgxYVJ6QlNlelNGbHpZajVkVGdtNTV2cEFUalV5akFUeWRWRXJoQ2RjM3l3VnNock5SNEgwIiB9";
-
-
-const PATH_PREFIX = "/" + TYPE + "/topic/";
-const ANCHOR = "div.postTopic";
-
-const postTopic = $("div.postTopic");
-const posterName = postTopic.find("strong").find('a.l').html();
-const posterId = postTopic.attr("data-item-user");
-const postId = getPostID();
-function getPostID() {
-    let path = document.URL.replace("https://" + document.domain, "");
-    const s = path.split("/");
-    return s[1] + "/topic/" + s[3];
-}
-//
-
-const STORAGE_KEY_PREFIX = TYPE + "_history_";
-const GREET = posterName + " 最近发表的其他" + WORD[TYPE];
-const GREET_fold = GREET + "&nbsp;&nbsp;► ";
-const GREET_expand = GREET + "&nbsp;&nbsp;▼ ";
-
-function fetchHistory(user) {
-    const key = STORAGE_KEY_PREFIX + user;
-    const cache = sessionStorage.getItem(key);
-    if (!cache) {
-        let url = atob(API_URL) + "user=" + user + "&type=" + TYPE;
-        $.ajax({
-            timeout: 8000,
-            crossDomain: true,
-            CORS: true,
-            dataType: 'json',
-            contentType: 'application/json',
-            type: 'POST',
-            url: url,
-            data: atob(PAYLOAD),
-            success: function (resp) {
-                console.log("[bgm_prevPosts] " + TYPE + " history for " + user + " fetched, " + resp.length + " records.");
-                sessionStorage.setItem(key, JSON.stringify(resp));
-                drawRecentDiv(genAList(resp));
-            },
-            error: function (resp) {
-                console.warn("[bgm_prevPosts] api fails");
-                const ERROR_TEXT = '<h2 id="history_loading" style="font-size:1.2em; text-align:right; margin-bottom:0px"><i>服务器正在ICU抢救中...</i></h2>';
-                drawRecentDiv(ERROR_TEXT);
-            }
-        });
-    } else {
-        console.log("[bgm_prevPosts] cache hit!");
-        drawRecentDiv(genAList(JSON.parse(cache)));
-    }
-}
-
-function shrinkDate(date) {
-    const s = date.split("-")
-    return Number(s[1]) + "/" + Number(s[2]) + "/" + Number(s[0].substr(2));
-}
-
-function genAList(histDict) {
-    const EMPTY_TEXT = '<h2 style="font-size:1.2em; text-align:right; margin-bottom:0px"><i>孤舟蓑笠翁，独钓寒江雪。</i></h2>';
-    let ret = '';
-    for (let i in histDict) {
-        const p = histDict[i];
-        // dont have to show current post in the post list
-        if (postId === p.type + "/topic/" + p.id) {
-            continue;
-        }
-        ret += '<a href="' + PATH_PREFIX + p.id + '" class="l"><span>' + p.title + '</sapn> <small class="grey">' + shrinkDate(p.lastpost) + '</small></a>';
-    }
-    if (ret == '') return EMPTY_TEXT;
-    return ret;
-}
-
-function drawRecentDiv(inner) {
-    const target = $("#historyList");
-    target.html(inner);
-}
-
-function initDraw() {
-    const text = '<h2 class="subtitle" id="slideIcon" style="font-size:1em; margin-bottom:0px;">' + GREET_fold + '</h2>';
-    const INIT_INNER = '<div id="historyList" style="margin-top:10px;" hidden><h2 id="history_loading" style="font-size:1.2em; margin-bottom:0px">loading...</h2></div>';
-    const father = $(ANCHOR);
-    father.append('<div class="subject_tag_section inner" id="historyWrapper" style="margin-top:8px;">' + text + INIT_INNER + '</div>');
-}
-
-initDraw();
-// fetchHistory(posterId);
-clickevent();
-
-
-function clickevent() {
-    var block = $('#historyWrapper');
-    block.live('click', function () {
-        var historyDiv = $(this).find('#historyList');
-        if (historyDiv.is(':hidden')) {
-            block.find('#slideIcon').html(GREET_expand);
-            if (block.find('#history_loading').html()) {
-                fetchHistory(posterId);
-            }
-            historyDiv.slideDown();
-        } else {
-            block.find('#slideIcon').html(GREET_fold);
-            historyDiv.slideUp();
-        }
-    });
-}
